1656024210dc3adae982e1bbfb87534d8828b9af
tex008 <texera009@gmail.com>
2024-01-08 18:21:41 -0300

    feat: create input component using composition pattern

--- src/app/componentes/SideBar/index.tsx
+++ src/app/componentes/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import { Cog, LifeBuoy, Search } from 'lucide-react'
+import * as Input from '../input'
 import MainNavigation from './MainNavigation'
 import NavItem from './MainNavigation/navItem'
 import Logo from './logo'
@@ -10,13 +11,12 @@ export default function SideBar() {
     <aside className="flex flex-col gap-6 border-r border-zinc-300  px-5 py-8 pb-12">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm ">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className=" flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h5- w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <MainNavigation />
 
