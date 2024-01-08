7aa80035acb1ad810c043445c1adeadbe49d6660
tex008 <texera009@gmail.com>
2024-01-08 14:51:43 -0300

    feat: create the navigation menu

--- src/app/componentes/SideBar/logo.tsx
+++ src/app/componentes/SideBar/logo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 export default function Logo() {
   return (
-    <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+    <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
       <svg
         width={38}
         height={38}
