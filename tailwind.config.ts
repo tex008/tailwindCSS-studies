6b45e662bc536223cb65166525530d4e8a47c26a
tex008 <texera009@gmail.com>
2024-01-07 23:42:10 -0300

    feat: create layout basic structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,13 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
